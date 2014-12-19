802af50121afbe84c56f343a5d2613b02714f6f6
Lorrayne Rocha <lrocha@fiksu.com>
2014-12-18 19:31:38 -0500

    Added the begining bits of the first level. In addition, built out some super useful things like valid_input, etc. Later, will pull this code out to act as an inheritor.

--- lib/game.rb
+++ lib/game.rb
@@ -3,12 +3,11 @@ require_relative 'usagi'
 require_relative 'gameplay'
 require_relative 'locations'
 
-class GameRunner
-	def initialize
-		@game = Gameplay.new(Usagi.new)
-	end
+Dir['../levels/*.rb'].each{ |f| require f }
 
+class GameRunner
 	def start
+		usa = Level1.new(Usagi.new, []).run
 		binding.pry
 	end
 end
