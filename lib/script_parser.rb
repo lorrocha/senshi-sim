d8fb1c4a3090b4b3502de667e49aee2bfff7735d
Lorrayne Rocha <lrocha@fiksu.com>
2014-12-22 22:40:06 -0500

    Begining the script parser so I can remove all the narrative from the actual level file and only keep the unique level elements within it.

---
+++ lib/script_parser.rb
@@ -0,0 +1,12 @@
+# Going under the assumption that scripts will follow the following format,
+
+# Luna: Usagi! You must transform
+# ** stat_check('composure',20)
+# Narrative: You throw your pen into the air and become a magical girl!
+
+class ScriptParser
+	def initialize(file)
+		@file = file
+	end
+
+end
