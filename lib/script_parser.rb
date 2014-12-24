26613b950bc4f151f40557fb4d1278a41abaf9c5
Lorrayne Rocha <lrocha@fiksu.com>
2014-12-23 22:14:41 -0500

    Need to work on the parser. Something isn't quite working here. It's not shucking the last ones into the script.

--- lib/script_parser.rb
+++ lib/script_parser.rb
@@ -7,6 +7,25 @@
 class ScriptParser
 	def initialize(file)
 		@file = File.new(file)
+		parse
+	end
+
+	def parse
+		total_script = []
+		temp_array = []
+		start_array = false
+
+		@file.each_line do |line|
+			temp_array << line if start_array && line != "\n"
+			if line.start_with?('Scene')
+				start_array = true
+			elsif line == "\n" || line == 'END'
+				total_script << temp_array
+				temp_array = []
+				start_array = false
+			end
+		end
+		binding.pry
 	end
 
 end
