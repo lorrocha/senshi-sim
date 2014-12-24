760510ad4ef0bcf50ca8dced6b5b550e06fa4f99
Lorrayne Rocha <lrocha@fiksu.com>
2014-12-23 21:20:58 -0500

    Going to add a test script. Also required the files more explicitely.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,6 @@
 require 'pry'
-require_relative 'usagi'
-require_relative 'gameplay'
-require_relative 'locations'
-
-Dir['../levels/*.rb'].each{ |f| require f }
+Dir['./*.rb'].each { |f| require f }
+Dir['../levels/*.rb'].each { |f| require f }
 
 class GameRunner
 	def start
