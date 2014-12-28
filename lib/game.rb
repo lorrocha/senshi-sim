d7d96aa24aa2c75e5c99fc05352b421c99657841
Lorrayne Rocha <lrocha@fiksu.com>
2014-12-28 17:38:49 -0500

    Not sure why it's repeating itself, but it is. And its annoooooying.

--- lib/game.rb
+++ lib/game.rb
@@ -3,9 +3,8 @@ Dir['./*.rb'].each { |f| require f }
 Dir['../levels/*.rb'].each { |f| require f }
 
 class GameRunner
-	def start
+	def self.start
 		usa = Level1.new(Usagi.new, []).run
 	end
 end
 
-GameRunner.new.start
