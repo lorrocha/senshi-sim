f654ce2ed00cd21e16a3fd156a0fad0bb8e6a725
Lorrayne Rocha <lrocha@fiksu.com>
2015-01-02 16:29:55 -0500

    Requiring the script files now.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require 'pry'
 Dir['./*.rb'].each { |f| require f unless f.include?('game.rb') }
 Dir['../levels/*.rb'].each { |f| require f }
+Dir['../scripts/*rb'].each { |f| require f }
 
 class GameRunner
 	def start
